6431a33d18642c28cd265f20bcae1a8fcd511f3a
Caner ÇAY <cay.cnr@gmail.com>
2018-08-02 09:55:31 +0300

    Bluetooth listener native module is not entegrated the project

--- README.md
+++ README.md
@@ -99,7 +99,7 @@ class App extends Component<{}> {
 | Method| Description |
 |:----------------------------------|:----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
 |**getCurrentState**|Returns an object, `type:on` if bluetooth is enabled, other ways disabled.|
-|**enable**|**Android only** Changes bluetooth state. Takes boolean parameter (defaults to true), `true` to enable, `false` to disable. Returns a promise, which returns whether the change was successful or not.|
+|**enable**|**Android only** Changes bluetooth state. Takes boolean parameter (defaults to true), `true` to enable, `false` to disable. Returns a promise, which returns whether the change was successful or not. Request on IOS.|
 |**disable**|**Android only** Disables bluetooth, same end result as calling `enable(false)`. Returns a promise, which returns whether the change was successful or not.|
 |**openBluetoothSettings**|**iOS only** Open OS Settings directly to bluetooth settings, recommended to use from Alert dialog, where user decides to change bluetooth state.|
 
--- index.js
+++ index.js
@@ -1,99 +1,105 @@
-import {NativeEventEmitter, NativeModules, Platform} from 'react-native';
+import {NativeEventEmitter, NativeModules, Platform} from "react-native";
 
 const {RNBluetoothListener} = NativeModules;
-const BluetoothInfoEventEmitter = new NativeEventEmitter(RNBluetoothListener);
 
+// console.log("RNBluetoothListener", RNBluetoothListener);
+let BluetoothManager = {};
 
-const DEVICE_CONNECTIVITY_EVENT = 'bluetoothDidUpdateState';
+if (RNBluetoothListener) {
+    const BluetoothInfoEventEmitter = new NativeEventEmitter(RNBluetoothListener);
+    const DEVICE_CONNECTIVITY_EVENT = "bluetoothDidUpdateState";
 
-const _subscriptions = new Map();
+    const _subscriptions = new Map();
 
-const BluetoothManager = {
+    BluetoothManager = {
 
-    addEventListener(eventName, handler): { remove: () => void } {
-        let listener;
-        if (eventName === 'change') {
-            listener = BluetoothInfoEventEmitter.addListener(
-                DEVICE_CONNECTIVITY_EVENT,
-                (appStateData) => {
-                    handler({
-                        type: appStateData,
-                    });
-                }
-            );
-        } else {
-            console.warn('Trying to subscribe to unknown event: "' + eventName + '"');
-            return {
-                remove: () => {
-                }
-            };
-        }
-
-        _subscriptions.set(handler, listener);
-        return {
-            remove: () => BluetoothManager.removeEventListener(eventName, handler)
-        };
-    },
-
-    /**
-     * Removes the listener for network status changes.
-     */
-    removeEventListener(eventName, handler) {
-        const listener = _subscriptions.get(handler);
-        if (!listener) {
-            return;
-        }
-        listener.remove();
-        _subscriptions.delete(handler);
-    },
-
-    /**
-     * Returns a promise that resolves to an object with `type key
-     */
-    getCurrentState() {
-        return RNBluetoothListener.getCurrentState().then(resp => {
-            return {
-                type: resp,
-            };
-        });
-    },
-
-    async enable(enabled: boolean = true) {
-        return new Promise((resolve, reject) => {
-            if (enabled) {
-                RNBluetoothListener.setBluetoothOn((error, done) => {
-                    if (error) {
-                        reject(error);
-                        return;
+        addEventListener(eventName, handler): { remove: () => void } {
+            let listener;
+            if (eventName === "change") {
+                listener = BluetoothInfoEventEmitter.addListener(
+                    DEVICE_CONNECTIVITY_EVENT,
+                    (appStateData) => {
+                        handler({
+                            type: appStateData,
+                        });
                     }
-                    resolve(done);
-                });
-            } else {
-                if (Platform.OS === 'android') {
-                    RNBluetoothListener.setBluetoothOff((error, done) => {
+                );
+            }
+            else {
+                console.warn("Trying to subscribe to unknown event: \"" + eventName + "\"");
+                return {
+                    remove: () => {
+                    }
+                };
+            }
+
+            _subscriptions.set(handler, listener);
+            return {
+                remove: () => BluetoothManager.removeEventListener(eventName, handler)
+            };
+        },
+
+        /**
+         * Removes the listener for network status changes.
+         */
+        removeEventListener(eventName, handler) {
+            const listener = _subscriptions.get(handler);
+            if (!listener) {
+                return;
+            }
+            listener.remove();
+            _subscriptions.delete(handler);
+        },
+
+        /**
+         * Returns a promise that resolves to an object with `type key
+         */
+        getCurrentState() {
+            return RNBluetoothListener.getCurrentState().then(resp => {
+                return {
+                    type: resp,
+                };
+            });
+        },
+
+        async enable(enabled: boolean = true) {
+            return new Promise((resolve, reject) => {
+                if (enabled) {
+                    RNBluetoothListener.setBluetoothOn((error, done) => {
                         if (error) {
                             reject(error);
                             return;
                         }
                         resolve(done);
                     });
-
                 }
+                else {
+                    if (Platform.OS === "android") {
+                        RNBluetoothListener.setBluetoothOff((error, done) => {
+                            if (error) {
+                                reject(error);
+                                return;
+                            }
+                            resolve(done);
+                        });
+
+                    }
+                }
+            });
+        },
+
+        async disable() {
+            return this.enable(false);
+        },
+
+        openBluetoothSettings() {
+            if (Platform.OS === "ios") {
+                RNBluetoothListener.openBluetoothSettings(() => {
+                })
             }
-        });
-    },
-
-    async disable() {
-        return this.enable(false);
-    },
-
-    openBluetoothSettings() {
-        if (Platform.OS === 'ios') {
-            RNBluetoothListener.openBluetoothSettings(() => {
-            })
         }
-    }
-};
+    };
+}
 
 export default BluetoothManager;
 
